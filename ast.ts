732942ff5bf9cbc7f4d5e770a5f4e055ebdaeb73
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2021-04-19 18:48:47 -0300

    Feat: added the Num and Id nodes to the AST

--- ast.ts
+++ ast.ts
@@ -1,3 +1,19 @@
 interface AST {
   equals(other: AST): boolean;
 }
+
+class Num implements AST {
+  constructor(public value: number) {}
+
+  equals(other: AST): boolean {
+    return other instanceof Num && this.value === other.value;
+  }
+}
+
+class Id implements AST {
+  constructor(public value: string) {}
+
+  equals(other: AST): boolean {
+    return other instanceof Id && this.value === other.value;
+  }
+}
