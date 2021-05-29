2a7be60417c59d6bc259b101e170e7510d1e0f59
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2021-05-29 12:43:50 -0300

    Feat: created parsers.ts Initially contains the Parser interface, as well as the ParserResult and Source class definitions

---
+++ parsers.ts
@@ -0,0 +1,25 @@
+class Source {
+  constructor(public string: string, public index: number) {}
+
+  match(regExp: RegExp): ParseResult<string> | null {
+    console.assert(regExp.sticky);
+    regExp.lastIndex = this.index;
+    let match = this.string.match(regExp);
+
+    if (match) {
+      let value = match[0];
+      let newIndex = this.index + value.length;
+      let source = new Source(this.string, newIndex);
+      return new ParseResult(value, source);
+    }
+    return null;
+  }
+}
+
+class ParseResult<T> {
+  constructor(public value: T, public source: Source) {}
+}
+
+interface Parser<T> {
+  parse(source: Source): ParseResult<T> | null;
+}
